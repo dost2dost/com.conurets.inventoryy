b1887c42eb2ed41f7f8cfd7c6d8e55b1ed0339c9
Nisar Hussain Ali <nisar.ali@conurets.com>
2019-03-15 16:49:33 +0500

    form and report pages

---
+++ src/main/resources/static/js/directive.js
@@ -0,0 +1,75 @@
+
+
+// Form left panel
+app.directive('formLeftPanel', function () {
+    return {
+        templateUrl: 'form-left-panel'
+    };
+});
+
+
+// Form left panel
+app.directive('basicInfo', function () {
+    return {
+        templateUrl: 'basic-info'
+    };
+});
+
+// Form left panel
+app.directive('storageInfo', function () {
+    return {
+        templateUrl: 'storage-info'
+    };
+});
+
+// Form left panel
+app.directive('itemHandling', function () {
+    return {
+        templateUrl: 'item-handling'
+    };
+});
+
+// Form left panel
+app.directive('caliration', function () {
+    return {
+        templateUrl: 'caliration'
+    };
+});
+
+// Form left panel
+app.directive('itemConition', function () {
+    return {
+        templateUrl: 'item-conition'
+    };
+});
+
+// Form left panel
+app.directive('supplierInfo', function () {
+    return {
+        templateUrl: 'supplier-info'
+    };
+});
+
+
+
+
+// Report left panel
+app.directive('reportLeftPanel', function () {
+    return {
+        templateUrl: 'report-left-panel'
+    };
+});
+
+// Report left panel
+app.directive('reportBasicInfo', function () {
+    return {
+        templateUrl: 'report-basic-info'
+    };
+});
+
+// Report left panel
+app.directive('spareUseage', function () {
+    return {
+        templateUrl: 'spare-useage'
+    };
+});
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,111 @@
+var app = angular.module("CIMS", ['ui.utils']);
+
+
+
+app.controller("form", function ($scope) {
+
+});
+
+
+
+app.controller('ctrl1', function($scope) {
+
+    $scope.data=[
+        [
+            "3/11/2019",
+            "System Architect",
+            "Edinburgh",
+            "Ali",
+            "Conure Telecom",
+            "Lorem Ipsum",
+            "61",
+            "4324324",
+            "34"
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+            "Edinburgh",
+            "Ali",
+            "Conure Telecom",
+            "Lorem Ipsum",
+            "61",
+            "4324324",
+            "34"
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+            "Edinburgh",
+            "Ali",
+            "Conure Telecom",
+            "Lorem Ipsum",
+            "61",
+            "4324324",
+            "34"
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+            "Edinburgh",
+            "Ali",
+            "Conure Telecom",
+            "Lorem Ipsum",
+            "61",
+            "4324324",
+            "34"
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+            "Edinburgh",
+            "Ali",
+            "Conure Telecom",
+            "Lorem Ipsum",
+            "61",
+            "4324324",
+            "34"
+        ],
+
+
+    ]
+
+
+});
+
+app.controller('ctrl2', function($scope) {
+
+    $scope.data2=[
+        [
+            "3/11/2019",
+            "System Architect",
+
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+
+        ],
+        [
+            "3/11/2019",
+            "System Architect",
+
+        ],
+
+
+
+    ]
+
+
+});
+
